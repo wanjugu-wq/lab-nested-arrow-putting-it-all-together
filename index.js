f10a6b56c14e5e32370eb904a6af7ec27482b055
Michelle Wanjugu <wanjuguzzz@gmail.com>
2026-04-03 18:34:54 +0300

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const createLoginTracker = (
+  userInfo = { Username: username, Password: password },
+) => {
+  let attemptCount = 0;
 
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  return (passwordAttempt = "") => {
+    if (attemptCount >= 3) {
+      return "Account locked due to too many failed login attempts";
+    } else if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else if (passwordAttempt !== userInfo.password) {
+      attemptCount++;
+      return "Attempt " + attemptCount + ": Login failed";
+    }
+  };
 };
+module.exports = {
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
+};
+
+console.log(createLoginTracker);
